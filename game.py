f84880a0a0be1c0cf2f7fc9b7e9b177659cd1bb7
Aurélien Chevalier <funigtor@me.com>
2014-12-20 14:46:52 +0100

    Apparition de la balle qui bouge
    Une balle bouge, c'est la base du jeu.

---
+++ game.py
@@ -0,0 +1,24 @@
+from Tkinter import *
+def animation():
+    global VX,VY,balleX,balleY
+    futurX,futurY = balleX+VX, balleY+VY
+    if futurX > 290 or futurX < 10:
+        VX = -VX
+        futurX = balleX + VX
+        changementCouleur()
+    if futurY > 190 or futurY < 10:
+        VY = -VY
+        changementCouleur()
+        futurY = balleY + VY
+    balleX, balleY = futurX, futurY
+    can.coords(balle,balleX - 10,balleY - 10, balleX + 10,balleY + 10)
+    fen.after(30,animation)
+fen = Tk()
+fen.geometry("300x300")
+can = Canvas(fen, width = 300,height = 200,bg="white")
+can.grid()
+VX,VY = 2,2
+balleX, balleY = 50,100
+balle = can.create_oval(40,90,60,110,fill="red")
+animation()
+fen.mainloop()
