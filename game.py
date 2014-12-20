2e8b3bed0a3f1206620142b0e8cca9cf1ac24565
Aurélien Chevalier <funigtor@me.com>
2014-12-20 17:47:28 +0100

    bugfix Hitbox associées aux mauvais rectangle
    Les hitbox du bas étaient celles du rectangle du haut et
    réciproquement, c'était perturbant.

--- game.py
+++ game.py
@@ -1,25 +1,31 @@
 from Tkinter import *
 def gaucheH(key):
     global gh,dh
-    gh=gh-5
-    dh=dh-5
-    can.coords(paveJoueurB,gh,190,dh,200)
+    gh=gh-10
+    dh=dh-10
+    can.coords(paveJoueurA,gh,10,dh,20)
+
 def droiteH(key):
     global dh,gh
-    gh=gh+5
-    dh=dh+5
-    can.coords(paveJoueurB,gh,190,dh,200)
+    gh=gh+10
+    dh=dh+10
+    can.coords(paveJoueurA,gh,10,dh,20)
+
 def gaucheB(key):
     global gb,db
-    gb=gb-5
-    db=db-5
-    can.coords(paveJoueurA,gb,10,db,20)
+    gb=gb-10
+    db=db-10
+    can.coords(paveJoueurB,gb,190,db,200)
 
 def droiteB(key):
     global db,gb
-    gb=gb+5
-    db=db+5
-    can.coords(paveJoueurA,gb,10,db,20)
+    gb=gb+10
+    db=db+10
+    can.coords(paveJoueurB,gb,190,db,200)
+
+def commencer(key):
+    animation()
+    lancement.config(text="")
 
 def animation():
     global VX,VY,balleX,balleY,gb,gh,dh,db,scoreHaut,scoreBas
@@ -64,17 +70,19 @@ can = Canvas(fen, width = 300,height = 200,bg="white")
 can.grid()
 scoreAfficheHaut = Label(fen,text = "Score joueur A:" + str(scoreHaut))
 scoreAfficheBas = Label(fen,text = "Score joueur B:" + str(scoreBas))
+lancement = Label(fen,text = "Appuyez sur c pour lancer le jeu")
 remiseEnJeu = Label(fen,text ="")
 scoreAfficheBas.grid()
 scoreAfficheHaut.grid()
 remiseEnJeu.grid()
+lancement.grid()
 VX,VY = 2,2
 balleX, balleY = 50,100
 balle = can.create_oval(40,90,60,110,fill="red")
 paveJoueurA = can.create_rectangle(gb,10,db,20,fill="blue")
 paveJoueurB = can.create_rectangle(gh,190,dh,200,fill="blue")
-animation()
 can.focus_set()
+can.bind("<c>",commencer)
 can.bind("<Left>",gaucheB)
 can.bind("<Right>",droiteB)
 can.bind("<q>",gaucheH)
