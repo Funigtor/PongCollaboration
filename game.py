fb81a8b8f63a7ef2b3ae3d3aaad29f498be460d3
Aurélien Chevalier <funigtor@me.com>
2014-12-20 17:21:56 +0100

    Début de la prise en charge de l'affichage du score
    WIP
    
    Il y a aussi un peu de gestion de la balle qui sort.

--- game.py
+++ game.py
@@ -4,8 +4,19 @@ def animation():
     futurX,futurY = balleX+VX, balleY+VY
     if (futurX > db or futurX < gb) and futurY > 190:
         scoreHaut += 1
+        scoreAfficheHaut.config(text = "Score joueur A:" + str(scoreHaut))
+        balleSortie = True
+        while balleSortie == True:
+            print 'prout'
+            #Ne rien faire
     if (futurX > dh or futurX < gh) and futurY < 10:
         scoreBas += 1
+        scoreAfficheBas.config(text = "Score joueur B:" + str(scoreBas))
+        remiseEnJeu.config(text="Appuyer sur X pour remettre en jeu")
+        balleSortie = True
+        while balleSortie == True:
+            print 'prout'
+            #Ne rien faire
     if futurX > 290 or futurX < 10:
         # S'applique aux murs gauche/droite
         VX = -VX
@@ -19,6 +30,7 @@ def animation():
     balleX, balleY = futurX, futurY
     can.coords(balle,balleX - 10,balleY - 10, balleX + 10,balleY + 10)
     fen.after(5,animation)
+
 scoreHaut = 0
 scoreBas = 0
 gh = 130
@@ -31,6 +43,10 @@ can = Canvas(fen, width = 300,height = 200,bg="white")
 can.grid()
 scoreAfficheHaut = Label(fen,text = "Score joueur A:" + str(scoreHaut))
 scoreAfficheBas = Label(fen,text = "Score joueur B:" + str(scoreBas))
+remiseEnJeu = Label(fen,text ="")
+scoreAfficheBas.grid()
+scoreAfficheHaut.grid()
+remiseEnJeu.grid()
 VX,VY = 2,2
 balleX, balleY = 50,100
 balle = can.create_oval(40,90,60,110,fill="red")
